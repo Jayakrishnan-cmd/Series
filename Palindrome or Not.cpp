41a5ee8ce4914f84572e597f6ab3ec3ea2895fdf
Jayakrishnan-cmd <59191150+Jayakrishnan-cmd@users.noreply.github.com>
2020-01-28 21:16:34 +0530

    Palindrome.cpp
    Study  how to check whether the given number is a palindrome

--- Palindrome or Not.cpp
+++ Palindrome or Not.cpp
@@ -1,1 +1,22 @@
-
+#include <iostream>
+using namespace std;
+int main()
+  {
+  int num, copy, digit, rev=0;
+  cout<<"Enter the number:";
+  cin>>num;
+  while(num!=0) 
+    {
+    digit=num%10;
+    rev=(rev*10)+digit;
+    num=num+10;
+    }
+  cout<<"The reverse of the number is:"<<rev;
+  if(rev==copy) 
+    cout<<"\n The given number is a palindrome ";
+  else
+    cout<<"\n It is not a palindrome";
+  return 0;
+  }
+    
+    
