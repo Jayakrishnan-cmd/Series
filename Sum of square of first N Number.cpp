d5054bfd8d4964290901dc5f7aa604273a600e44
Jayakrishnan-cmd <59191150+Jayakrishnan-cmd@users.noreply.github.com>
2020-01-28 20:12:49 +0530

    Sum of number of N Natural Number.cpp
    To find the sum of squares of First N Natural numbers.

--- Sum of square of first N Number.cpp
+++ Sum of square of first N Number.cpp
@@ -1,1 +1,16 @@
-#imclude 
+#include <iostream>
+using namespace std;
+int main() 
+  {
+  int num=0, i=0;
+  cout<<"Enter the limit:";
+  cin>>num;
+  for(i=0;i<=num;i++) 
+    {
+    sum=sum+(i*i) ;
+    }
+  cout<<"\n The sum of square of the number";
+  cout<<"upto"<<num<<"is";
+  cout<<sum;
+  return 0;
+  }
