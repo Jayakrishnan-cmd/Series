666ed9a7c3187e3c2f5c6c04e7bb056eb3f46a58
Jayakrishnan-cmd <59191150+Jayakrishnan-cmd@users.noreply.github.com>
2020-01-28 20:50:19 +0530

    Factorial.cpp
    Study how to find the factorial of a number.

--- Factorial of a number.cpp
+++ Factorial of a number.cpp
@@ -1,2 +1,17 @@
 #include <iostream>
-using 
+using namespace std;
+int main()
+  { 
+  int f=1;
+  for (int i=1;i<=n;i++) 
+    f*=i;
+  return f;
+  }
+int main()
+  {
+  int n;
+  cout<<"Enter the number:";
+  cin<<n;
+  cout<<"factorial:"<<factorial (n) ;
+  return 0;
+  }
